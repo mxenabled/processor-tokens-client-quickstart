5937ede00bc6ccb1f108ff3114be29d30c76cdce
Logan Rasmussen <logan.rasmussen@mx.com>
2022-02-11 12:23:53 -0700

    Add MX External ID via the API

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,18 @@ class UsersController < ApplicationController
     puts(params)
     @user = User.new(user_params)
 
+    # Attempt to get an external id from MX
+    external_response = @user.generate_external_id
+
+    if external_response.user.guid
+      @user.external_id = external_response.user.guid
+    end
+
     if @user.save
+      if !@user.external_id
+        puts "Warning: external_id was NOT created for #{@user.id}"
+      end
+
       redirect_to @user
     else
       render :new, status: :unprocessable_entity
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,22 @@
 class User < ApplicationRecord
     validates :name, presence: true, length: {minimum: 3}
     validates :password, presence: true, length: {minimum: 10}
+
+    def generate_external_id
+        api_client = ::MxPlatformRuby::ApiClient.new
+        api_client.default_headers['Accept'] = 'application/vnd.mx.api.v1+json'
+        mx_platform_api = ::MxPlatformRuby::MxPlatformApi.new(api_client)
+
+        request_body = ::MxPlatformRuby::UserCreateRequestBody.new(
+            user: ::MxPlatformRuby::UserCreateRequest.new(
+                metadata: "Create a test user!"
+            )
+        )
+
+        begin
+            response = mx_platform_api.create_user(request_body)
+        rescue ::MxPlatformRuby::ApiError => e
+            puts "Error when calling MxPlatformApi->create_user: #{e}"
+        end
+    end
 end
